327a479270f24f7e3b14d71d0726045470146fed
dmitry741 <dmitrypavlov74@gmail.com>
2018-11-12 16:26:32 +0300

    new class for transformation
    ridan

--- Simple3D/Simple3D/TransformEngine.cs
+++ Simple3D/Simple3D/TransformEngine.cs
@@ -6,79 +6,55 @@ using System.Threading.Tasks;
 
 namespace Simple3D
 {
-    class TransformEngine : ITransformEngine
+    class TransformEngine
     {
-        List<Abstract3DInstance> _list = new List<Abstract3DInstance>();
-
-        public void Add(Abstract3DInstance item)
-        {
-            _list.Add(item);
-        }
-
-        public void Clear()
-        {
-            _list.Clear();
-        }
-
-        public void RotateXZ(double angle, double Xc, double Zc)
+        static public void RotateXZ(Abstract3DInstance item, double angle, double Xc, double Zc)
         {
             double si = Math.Sin(angle);
             double co = Math.Cos(angle);
 
-            foreach (Abstract3DInstance items in _list)
+            foreach (Point3D point in item.Points)
             {
-                foreach (Point3D point in items.Points)
-                {
-                    double x = co * (point.X - Xc) - si * (point.Z - Zc) + Xc;
-                    double z = si * (point.X - Xc) + co * (point.Z - Zc) + Zc;
+                double x = co * (point.X - Xc) - si * (point.Z - Zc) + Xc;
+                double z = si * (point.X - Xc) + co * (point.Z - Zc) + Zc;
 
-                    point.X = x;
-                    point.Z = z;
-                }
+                point.X = x;
+                point.Z = z;
             }
         }
 
-        public void RotateYZ(double angle, double Yc, double Zc)
+        static public void RotateYZ(Abstract3DInstance item, double angle, double Yc, double Zc)
         {
             double si = Math.Sin(angle);
             double co = Math.Cos(angle);
 
-            foreach (Abstract3DInstance items in _list)
+            foreach (Point3D point in item.Points)
             {
-                foreach (Point3D point in items.Points)
-                {
-                    double y = co * (point.Y - Yc) - si * (point.Z - Zc) + Yc;
-                    double z = si * (point.Y - Yc) + co * (point.Z - Zc) + Zc;
+                double y = co * (point.Y - Yc) - si * (point.Z - Zc) + Yc;
+                double z = si * (point.Y - Yc) + co * (point.Z - Zc) + Zc;
 
-                    point.X = y;
-                    point.Z = z;
-                }
+                point.Y = y;
+                point.Z = z;
             }
         }
 
-        public void Scale(double scaleFactor, double Xc, double Yc, double Zc)
+        static public void Scale(Abstract3DInstance item, double scaleFactor, double Xc, double Yc, double Zc)
         {
-            foreach (Abstract3DInstance items in _list)
+            foreach (Point3D point in item.Points)
             {
-                foreach (Point3D point in items.Points)
-                {
-                    point.X = scaleFactor * (point.X - Xc) + Xc;
-                    point.Y = scaleFactor * (point.Y - Yc) + Yc;
-                    point.Z = scaleFactor * (point.Z - Zc) + Zc;
-                }
+                point.X = scaleFactor * (point.X - Xc) + Xc;
+                point.Y = scaleFactor * (point.Y - Yc) + Yc;
+                point.Z = scaleFactor * (point.Z - Zc) + Zc;
             }
         }
 
-        public void Translate(double X, double Y, double Z)
+        static public void Translate(Abstract3DInstance item, double X, double Y, double Z)
         {
-            foreach(Abstract3DInstance items in _list)
+            foreach(Point3D point in item.Points)
             {
-                foreach(Point3D point in items.Points)
-                {
-                    point.X += X;
-                    point.Y += Y;
-                    point.Z += Z;
-                }
+                point.X += X;
+                point.Y += Y;
+                point.Z += Z;
             }
         }
     }
